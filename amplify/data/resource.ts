2de753046e475eb8318150af894a394962c71019
BDillman19 <36203523+BDillman19@users.noreply.github.com>
2026-06-22 17:43:26 -0500

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -46,7 +46,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    //defaultAuthorizationMode: "userPool",
+    defaultAuthorizationMode: "userPool",
    // defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
    // apiKeyAuthorizationMode: {
